d4d1536e4ecb85d0b0773fac881513cfc73bbc1b
Sergey <serg9205@gmail.com>
2024-02-27 13:11:31 +0200

    fix: configurate vue.config

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 });
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/hotel-booking-calendar/" : "/",
 };
